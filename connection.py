b16339682936747267295a5c02a93c1aff363d91
Ualas <ualasroh@gmail.com>
2019-07-31 12:46:59 -0700

    Independent Functions for database connection

--- connection.py
+++ connection.py
@@ -1,6 +1,5 @@
 import psycopg2
 import os
-from pprint import pprint
 import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
@@ -10,45 +9,48 @@ DBUSER = os.getenv('DBUSER')
 DBHOST = os.getenv('DBHOST')
 DBPASS = os.getenv('DBPASS')
 
-def getData(report):
+def connectionDB(dbname):
+    con = psycopg2.connect(database=dbname, user=DBUSER, password=DBPASS, host=DBHOST, port='5432')
+    con.set_client_encoding('UTF8')
+    return(con)
+
+def disconnectDB(con):
+    con.close()
+
+def getData(con,report):
     data = []
     dbtables = []
-    con = psycopg2.connect(database='ir_rio', user=DBUSER, password=DBPASS, host=DBHOST, port='5432')
-
-    con.set_client_encoding('UTF8')
-    cur = con.cursor()
-    cur.execute("SELECT schemaname,viewname FROM pg_catalog.pg_views WHERE viewname LIKE '%evw'")
-    rows = cur.fetchall()
-
-    for row in rows:
-        dbtables.append({"schema" : row[0], "table" : row[1]})
-
-    df_dbtables = pd.DataFrame(dbtables)
+    dbs = []
     cur = con.cursor()
 
-    for index, row in df_dbtables.iterrows():
-        table = row['table']
-        schema = row['schema']
-        schema_table = schema + "." + table
-        try:
-            if (report == 'styles'):
-                if not ("extent" in table or "cone" in table):
-                    cur.execute("SELECT type,stylename,COUNT(objectid) FROM %s GROUP BY type,stylename" % schema_table)
+    if report == 'databases':
+        cur.execute("SELECT datname FROM pg_database WHERE datistemplate = false and datname != 'postgres';")
+        rows = cur.fetchall()
+        for row in rows:
+            data.append({row[0]})
+    else:
+        cur.execute("SELECT schemaname,viewname FROM pg_catalog.pg_views WHERE viewname LIKE '%evw'")
+        rows = cur.fetchall()
+        for row in rows:
+            dbtables.append({"schema" : row[0], "table" : row[1]})
+        df_dbtables = pd.DataFrame(dbtables)
+
+        for index, row in df_dbtables.iterrows():
+            table = row['table']
+            schema = row['schema']
+            schema_table = schema + "." + table
+            try:
+                if (report == 'styles'):
+                    if not ("extent" in table or "cone" in table):
+                        cur.execute("SELECT type,stylename,COUNT(objectid) FROM %s GROUP BY type,stylename" % schema_table)
+                        rs = cur.fetchall()
+                        for r in rs:
+                            data.append({"Table" : row['table'][:-4], "Type" : r[0], "Stylename" : r[1], "Count" : r[2]})
+                elif report == 'layers':
+                    cur.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '%s' AND table_schema = '%s'" % (table, schema))
                     rs = cur.fetchall()
                     for r in rs:
-                        data.append({"Table" : row['table'][:-4], "Type" : r[0], "Stylename" : r[1], "Count" : r[2]})
-            elif report == 'layers':
-                cur.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '%s' AND table_schema = '%s'" % (table, schema))
-                rs = cur.fetchall()
-                for r in rs:
-                    data.append({"Table" : table[:-4] ,"Field" : r[0], "Type" : r[1]})
-        #except: continue
-        except psycopg2.OperationalError: traceback.print_exc()
-    con.close()
-    #pprint(data)
-    return(data)
+                        data.append({"Table" : table[:-4] ,"Field" : r[0], "Type" : r[1]})
+            except psycopg2.OperationalError: traceback.print_exc()
 
-#df = pd.DataFrame(data, columns=['Table', 'Type', 'Stylename'])
-#pt = pd.pivot_table(df,index=["Table","Type","Stylename"], aggfunc=len) #FIX ISSUE HERE
-#data = getData()
-#pprint(data)
+    return(data)
