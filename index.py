1cb0ee43502854a1146f3f0b57464f1a3d9ab74f
Ualas <ualasroh@gmail.com>
2019-07-31 12:47:22 -0700

    Added boostrap table

--- index.py
+++ index.py
@@ -1,22 +1,74 @@
 from flask import *
 from connection import getData
+from connection import connectionDB
+from connection import disconnectDB
 from flask import jsonify
 import pandas as pd
+from pprint import pprint
 
 app = Flask(__name__)
 
+layers_columns = [
+{
+"field": "Table",
+"title": "FeatureClass",
+"sortable": True,
+},
+  {
+    "field": "Field", # which is the field's name of data key
+    "title": "Field", # display as the table header's name
+    "sortable": True,
+  },
+  {
+    "field": "Type",
+    "title": "Type",
+    "sortable": True,
+  }
+]
+styles_columns = [
+{
+"field": "Table",
+"title": "FeatureClass",
+"sortable": True,
+},
+  {
+    "field": "Type", # which is the field's name of data key
+    "title": "Type", # display as the table header's name
+    "sortable": True,
+  },
+  {
+    "field": "Stylename",
+    "title": "Stylename",
+    "sortable": True,
+  },
+    {
+      "field": "Count",
+      "title": "Count",
+      "sortable": True,
+    }
+]
+
 @app.route('/')
 def index():
-    layers_data = getData("layers")
+    dbname = request.args.get('db')
+    if not dbname:
+        dbname = "ir_rio"
+
+    con = connectionDB(dbname)
+
+    databases_data = getData(con,"databases")
+
+    layers_data = getData(con,"layers")
     layers_df = pd.DataFrame(layers_data, columns=['Table','Field', 'Type'])
     layers_df.set_index(['Table'], inplace=True)
 
-    styles_data = getData("styles")
+    styles_data = getData(con,"styles")
     styles_df = pd.DataFrame(styles_data, columns=['Table','Type', 'Stylename', 'Count'])
     styles_df.set_index(['Table'], inplace=True)
 
-    return render_template('view.html',tables=[styles_df.to_html(classes='styles'),layers_df.to_html(classes='layers')],
-    titles = ['na','Attributes','Fields'])
+    disconnectDB(con)
+
+    return render_template('table.html',data=styles_data, columns=styles_columns, title='Table', databases=databases_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
